2955847d44f243469b185e44e3071e88a412d73d
Duck <duck8bananachips@outlook.com>
2025-09-12 21:24:51 +0700

    Add exercise main.cpp

--- exercises/test_file.cpp
+++ exercises/test_file.cpp
@@ -1,18 +1,7 @@
-param(
-  [string]$sourceFile = "D:\C++ CODE\test_file.cpp",
-  [string]$repoPath = "D:\LOCAL_GITHUB_REPOSITORIES\GraphTheory",
-  [string]$destRelative = "exercises",   # thư mục trong repo
-  [string]$message = "Add exercise main.cpp"
-)
+#include <bits/stdc++.h>
 
-$destDir = Join-Path $repoPath $destRelative
-if (-not (Test-Path $destDir)) { New-Item -ItemType Directory -Path $destDir -Force | Out-Null }
-
-Copy-Item -Path $sourceFile -Destination $destDir -Force
-
-Push-Location $repoPath
-$filename = Split-Path $sourceFile -Leaf
-git add (Join-Path $destRelative $filename)
-git commit -m $message
-git push origin develop
-Pop-Location
+int main()
+{
+  printf("hello cu");
+  return 0;
+}
